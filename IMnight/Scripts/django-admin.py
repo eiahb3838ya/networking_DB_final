3556a67c042aa3ecb530ca293c487132780ca9eb
eiahb3838ya <eiahb3838ya@gmail.com>
2017-12-30 16:16:01 +0800

    go

---
+++ IMnight/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\test\documents\github\networking_db_final\imnight\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
